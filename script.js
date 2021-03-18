d02b5278a2f19610d90a84e546796a89e6a5e8cd
Adri Ana <szopiangreg@gmail.com>
2021-03-18 14:01:38 +0000

    commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const slider = document.querySelector(".slider-container"),
+  slides = Array.form(document.querySelectorAll(".slide"));
+
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID = 0,
+  currentIndex = 0;
+
+slides.forEach((slide, index) => {
+  // DRGGING IMAGE STOP EFECT
+  const slideImage = slide.querySelector("img");
+  slideImage.addEventListener("dragstart", (e) => e.preventDefault());
+
+  //Touch event
+  slide.addEventListener("touchstart", touchStart(index));
+  slide.addEventListener("touchend", touchEnd);
+  slide.addEventListener("touchmove", touchMove);
+
+  // Mouse event
+  slide.addEventListener("mousedown", touchStart(index));
+  slide.addEventListener("mouseup", touchEnd);
+  slide.addEventListener("mouseleave", touchEnd);
+  slide.addEventListener("mousemove", touchMove);
+});
+
+function touchStart(index) {
+  return function (event) {
+    console.log("start");
+  };
+}
+
+function touchEnd() {
+  console.log("end");
+}
+
+function touchMove() {
+  console.log("move");
+}
